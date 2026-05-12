2ea42704c0ef5e4049255ac439d40d81c3768189
MustafaQal <salem.msms@gmail.com>
2026-05-12 12:16:19 +0300

    Products: UI/UX

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,8 +51,8 @@ const displayProducts = async () => {
     const products = data.map((product) => {
         return `
              <!-- Mustafa's Creations hhh -->
-              
-                <div>
+                <!--
+                <div class="bg-white shadow">
                     <h3 class="" >${product.title} </h3>
                     <span class="price"> Rate ${product.price} </span>
                     <span class="rating"> Rate ${product.rating} </span>
@@ -60,6 +60,28 @@ const displayProducts = async () => {
                         <img src="${product.thumbnail}"/>
                     </div>
                 </div>
+                -->
+            <!--Asked some help with UI/UX :) hhhhhh-->
+
+            <div class="bg-white rounded-2xl shadow-sm hover:shadow-xl transition-all duration-300 overflow-hidden group">
+                <div class=" h-48 overflow-hidden bg-gray-100">
+                    <img 
+                        src="${product.thumbnail}" 
+                        alt="${product.title}"
+                        class="w-full h-full object-cover group-hover:scale-105 transition"
+                    />
+                </div>
+
+                <div class="p-5">
+                    <h3 class="font-semibold text-lg text-gray-800 mb-2"> ${product.title} </h3>
+
+                    <div class="flex items-center justify-between mb-3">
+                        <span class="text-blue-600 font-bold text-lg">$${product.price}</span>
+
+                        <span class="text-yellow-500 text-sm font-medium">⭐ ${product.rating} </span>
+                    </div>
+                </div>
+            </div>
             
         `
     }).join("");
