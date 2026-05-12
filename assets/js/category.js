4e7fa9fc7d6c70959c85916a043f14138374886a
MustafaQal <salem.msms@gmail.com>
2026-05-12 18:08:08 +0300

    displayProductsCategory: UI/UX

--- assets/js/category.js
+++ assets/js/category.js
@@ -23,16 +23,22 @@ const displayList = async () => {
     const productsList = listCategory.map((product) => {
         return ` 
             <!-- Mustafa Copy the same code from displayProducts hhhhhh :) -->
-             <!-- Mustafa's Creations hhh -->
-            
-                <div class="bg-white shadow card">
-                    <h3 class="" >${product.title} </h3>
-                    <span class="price"> Rate ${product.price} </span>
-                    <span class="rating"> Rate ${product.rating} </span>
-                    <div class="thumbnail"> 
-                        <img src="${product.thumbnail}"/>
+<div class="bg-white rounded-2xl shadow-sm hover:shadow-xl transition-all duration-300  group">
+                <div class=" h-52 bg-gray-100">
+                    <img class="max-w-full max-h-full object-contain group-hover:scale-105 transition"
+                        src="${product.thumbnail}" 
+                        alt="${product.title}"
+                    />
+                </div>
+
+                <div class="p-5">
+                    <h3 class="font-semibold text-lg text-gray-800 mb-2"> ${product.title} </h3>
+                    <div class="flex items-center justify-between mb-3">
+                        <span class="text-blue-600 font-bold text-lg">$${product.price}</span>
+                        <span class="text-yellow-500 text-sm font-medium">⭐ ${product.rating} </span>
                     </div>
                 </div>
+            </div>
             `
 
     }).join("");
